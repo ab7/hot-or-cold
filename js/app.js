c98869638798852a5a15f03b067684b49ffe3cfa
Alex Baker <robotsmack@gmail.com>
2014-04-09 11:19:02 -0600

    added hint animations

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ $(function(){
   function intructions() {
     $('#howTo').click(function() {
       $('#inputWrapper').hide();
+      $('nav').hide();
       $('#instructions').fadeIn(1000);
     });
-    $('.close').click(function() {
+    $('#closeInstruc').click(function() {
       $('#instructions').hide();
+      $('nav').fadeIn(1000);
       $('#inputWrapper').fadeIn(1000);
     });
   }
@@ -14,7 +16,8 @@ $(function(){
   // start a new game
   function newGame() {
     $('#correct').hide();
-    $('.userGuess').prop('placeholder', 'Enter your Guess');
+    $('#redBox').animate({width: '20px'}, 'slow');
+    $('.userGuess').prop('placeholder', 'Enter');
     $('.userGuess').prop('disabled', false);
     $('.userGuess').focus();
     numToGuess = Math.floor((Math.random() * 100) + 1);
@@ -24,22 +27,28 @@ $(function(){
         if (guess <= 100 && guess > 0) {
           var howFar = Math.abs(guess - numToGuess);
           if (guess === numToGuess) {
+            $('#redBox').animate({width: '785px'}, 'slow');
             $('#correct').fadeIn();
             $('.userGuess').prop('disabled', true);
-            $('.userGuess').prop('placeholder', 'Good Job!');
+            $('.userGuess').prop('placeholder', 'Great!');
           } else if (howFar <= 10) {
-            $('#redBox').animate({width: '500px'}, 'slow');
+            $('#veryHotHint').fadeIn().finish().fadeOut(1000);
+            $('#redBox').animate({width: '718px'}, 'slow');
           } else if (howFar <= 20 && howFar > 10) {
-            $('#redBox').animate({width: '500px'}, 'slow');
+            $('#hotHint').fadeIn().finish().fadeOut(1000);
+            $('#redBox').animate({width: '630px'}, 'slow');
           } else if (howFar <= 30 && howFar > 20) {
-            $('#redBox').animate({width: '500px'}, 'slow');
+            $('#warmHint').fadeIn().finish().fadeOut(1000);
+            $('#redBox').animate({width: '475px'}, 'slow');
           } else if (howFar <= 50 && howFar > 30) {
-            $('#redBox').animate({width: '500px'}, 'slow');
+            $('#coldHint').fadeIn().finish().fadeOut(1000);
+            $('#redBox').animate({width: '295px'}, 'slow');
           } else {
-            $('#redBox').animate({width: '500px'}, 'slow');
+            $('#iceColdHint').fadeIn().finish().fadeOut(1000);
+            $('#redBox').animate({width: '66px'}, 'slow');
           }
         } else {
-          $('#error').fadeIn().finish().fadeOut(2000);
+          $('#error').fadeIn().finish().fadeOut(1000);
         }
         $(this).val("");
       }
